11a40bf90d9e559deba13e1f95b57ccb9ae73640
beckdaniel <daniel.e.beck@gmail.com>
2016-03-23 18:29:43 +0000

    added numpy vectorized implementation

--- flakes/string/string_kernel.py
+++ flakes/string/string_kernel.py
@@ -7,15 +7,19 @@ class StringKernel(object):
     A general class for String Kernels.
     """
 
-    def __init__(self, decay=1.0, order_coefs=[1.0], slow=False,
+    def __init__(self, decay=1.0, order_coefs=[1.0], mode='tf',
                  sim='hard'):
         self.decay = decay
         self.order_coefs = order_coefs
         self.order = len(order_coefs)
-        if slow:
+
+        if mode == 'slow':
             self.k = self._k_slow
-        else:
-            self.k = self._k
+        elif mode == 'numpy':
+            self.k = self._k_numpy
+        elif mode == 'tf':
+            self.k = self._k_tf
+
         if sim == 'hard':
             self.sim = sims.hard_match
 
@@ -42,7 +46,8 @@ class StringKernel(object):
             for j in xrange(n - 1):
                 Kpp = 0.0
                 for k in xrange(m - 1):
-                    Kpp = decay * Kpp + decay * decay * sim(s1[j], s2[k]) * Kp[i][j][k]
+                    Kpp = (decay * Kpp + 
+                           decay * decay * sim(s1[j], s2[k]) * Kp[i][j][k])
                     Kp[i + 1][j + 1][k + 1] = decay * Kp[i + 1][j][k + 1] + Kpp
                     result = 0.0
         for i in xrange(order):
@@ -54,6 +59,45 @@ class StringKernel(object):
 
         return result
 
-    def _k(self, s1, s2):
-        pass
+    def _k_numpy(self, s1, s2):
+        """
+        Calculates k over two strings. Inputs can be strings or lists.
+        This is a vectorized version using numpy.
+        """
+        n = len(s1)
+        m = len(s2)
+        decay = self.decay
+        order = self.order
+        sim = self.sim
+        coefs = self.order_coefs
+
+        Kp = np.zeros(shape=(order + 1, n, m))
+        Kp[0,:,:] = 1.0
+        result = 0.0
+
+        # Store sim(j, k) values
+        S = np.zeros(shape=(n,m))
+        for j in xrange(n):
+            for k in xrange(m):
+                S[j,k] = sim(s1[j], s2[k])
         
+        # Triangular matrix over decay powers
+        max_len = max(n, m)
+        D = np.zeros((max_len, max_len))
+        d1, d2 = np.indices(D.shape)
+        for k in xrange(max_len):
+            D[d2-k == d1] = decay ** k 
+
+        # Initilazing auxiliary variables
+        Kpp = np.zeros(shape=(order, n, m))
+        decay_sq = decay * decay
+
+        for i in xrange(order):
+            Kpp[i, :-1, 1:] = decay_sq* (S[:-1,:-1] * Kp[i,:-1,:-1]).dot(D[1:m, 1:m])
+            Kp[i + 1, 1:] = Kpp[i, :-1].T.dot(D[1:n, 1:n]).T
+        
+        # Final calculation
+        Ki = np.sum(np.sum(S * Kp[:-1], axis=1), axis=1) * decay_sq
+        #Ki = np.sum(S * np.sum(Kp, axis=0), axis=0) * decay_sq
+
+        return Ki.dot(coefs)
--- flakes/tests/test_string.py
+++ flakes/tests/test_string.py
@@ -7,15 +7,23 @@ class StringKernelTests(unittest.TestCase):
     def setUp(self):
         self.s1 = 'cata'
         self.s2 = 'gatta'
-        self.k_slow = flakes.string.StringKernel(slow=True)
-        self.k_fast = flakes.string.StringKernel()
+        self.k_slow = flakes.string.StringKernel(mode='slow')
+        self.k_np = flakes.string.StringKernel(mode='numpy')
         
-    def test_sk_1(self):
+    def test_sk_slow_1(self):
         self.k_slow.order = 5
         self.k_slow.order_coefs = [1.] * 5
         self.k_slow.decay = 2.0
         expected = 504.0
         result = self.k_slow.k(self.s1, self.s2)
+        self.assertAlmostEqual(result, expected)
+
+    def test_sk_numpy_1(self):
+        self.k_np.order = 5
+        self.k_np.order_coefs = [1.] * 5
+        self.k_np.decay = 2.0
+        expected = 504.0
+        result = self.k_np.k(self.s1, self.s2)
         self.assertAlmostEqual(result, expected)
         
 
