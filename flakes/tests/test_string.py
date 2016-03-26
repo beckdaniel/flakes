6018691ab9d8d60a5aaa2481ae55f962a57e12e9
beckdaniel <daniel.e.beck@gmail.com>
2016-03-26 19:21:19 +0000

    a few more quirky tests, have to account for float32 and float64 differences

--- flakes/tests/test_string.py
+++ flakes/tests/test_string.py
@@ -7,6 +7,8 @@ class StringKernelTests(unittest.TestCase):
     def setUp(self):
         self.s1 = 'cata'
         self.s2 = 'gatta'
+        self.s3 = 'cgtagctagcgacgcagccaatcgatcg'
+        self.s4 = 'cgagatgccaatagagagagcgctgta'
         self.k_slow = flakes.string.StringKernel(mode='slow')
         self.k_np = flakes.string.StringKernel(mode='numpy')
         self.k_tf = flakes.string.StringKernel()
@@ -55,6 +57,30 @@ class StringKernelTests(unittest.TestCase):
         self.k_np.decay = 0.8
         result2 = self.k_np.k(self.s1, self.s2)
         self.assertAlmostEqual(result1, result2)
+
+    def test_compare_2(self):
+        self.k_tf.order = 5
+        self.k_tf.order_coefs = [0.1, 0.2, 0.4, 0.5, 0.7]
+        self.k_tf.decay = 0.8
+        result1 = self.k_tf.k(self.s3, self.s4)
+
+        self.k_np.order = 5
+        self.k_np.order_coefs = [0.1, 0.2, 0.4, 0.5, 0.7]
+        self.k_np.decay = 0.8
+        result2 = self.k_np.k(self.s3, self.s4)
+        self.assertAlmostEqual(result1, result2, places=2)
+
+    def test_compare_3(self):
+        self.k_tf.order = 5
+        self.k_tf.order_coefs = [0.1, 0.2, 0.4, 0.5, 0.7]
+        self.k_tf.decay = 0.8
+        result1 = self.k_tf.k(self.s1, self.s4)
+
+        self.k_np.order = 5
+        self.k_np.order_coefs = [0.1, 0.2, 0.4, 0.5, 0.7]
+        self.k_np.decay = 0.8
+        result2 = self.k_np.k(self.s1, self.s4)
+        self.assertAlmostEqual(result1, result2, places=2)
         
 
 if __name__ == "__main__":
