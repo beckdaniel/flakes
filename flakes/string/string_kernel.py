fixed gradients, must do grad check

flakes/string/string_kernel.py
@@ -360,9 +360,17 @@ class StringKernel(object):
             s2 = self._build_input_matrix(s2, self.maxlen)
         else:
             s2 = s2.T
+        if isinstance(self.gap_decay, np.ndarray):
+            gap = self.gap_decay[0]
+        else:
+            gap = self.gap_decay
+        if isinstance(self.match_decay, np.ndarray):
+            match = self.match_decay[0]
+        else:
+            match = self.match_decay
         feed_dict = {self._mat1: s1, self._mat2: s2,
-                     self._gap: self.gap_decay, 
-                     self._match: self.match_decay,
+                     self._gap: gap, 
+                     self._match: match,
                      self._coefs: np.array(self.order_coefs)[None, :]}
         output = self.sess.run(self.result, feed_dict=feed_dict)
         return output

flakes/wrappers/gpy.py
@@ -29,17 +29,27 @@ class GPyStringKernel(StringKernel, Kern):
         self.link_parameter(self.gap_decay)
         self.link_parameter(self.match_decay)
         self.link_parameter(self.order_coefs)
-        self.gap_decay.constrain_fixed(gap_decay)
-        self.match_decay.constrain_fixed(match_decay)
-        self.order_coefs.constrain_fixed(order_coefs)
+        #self.gap_decay.constrain_fixed(gap_decay)
+        #self.match_decay.constrain_fixed(match_decay)
+        #self.order_coefs.constrain_fixed(order_coefs)
 
     def update_gradients_full(self, dL_dK, X, X2=None):
         if X2 is None: 
             dL_dK = (dL_dK+dL_dK.T)/2
         self.gap_decay.gradient = np.sum(self.gap_grads * dL_dK)
         self.match_decay.gradient = np.sum(self.match_grads * dL_dK)
-        for i in xrange(order):
-            self.order_coefs[i].gradient = np.sum(self.coef_grads[:, :, i] * dL_dK)
+        for i in xrange(self.order):
+            self.order_coefs.gradient[i] = np.sum(self.coef_grads[:, :, i] * dL_dK)
+        #print ''
+        #print 'GAP GRADS'
+        #print self.gap_grads
+        #print ''
+        #print 'MATCH GRADS'
+        #print self.match_grads
+        #print ''
+        #print 'DLDK'
+        #print dL_dK
+        #print ''
 
 
     def _K(self, X, X2=None):
@@ -76,6 +86,4 @@ class GPyStringKernel(StringKernel, Kern):
             result[i] = self.K(x1[0], x1[0])
         return result
 
-    def update_gradients_full(self, dL_dK, X, X2=None):
-        pass
         