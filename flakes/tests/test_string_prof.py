ca3de275119ffe1ae873547069a9bf026234aa10
Daniel Beck <daniel.e.beck@gmail.com>
2016-09-02 16:39:43 +0100

    added a long string test

--- flakes/tests/test_string_prof.py
+++ flakes/tests/test_string_prof.py
@@ -16,13 +16,16 @@ class StringKernelProfiling(unittest.TestCase):
         self.s2 = "To say he shall leave a wall of strong Towns behind him is to say nothing at all in this case, while there is an Army of 60000 Men in the field there; to say he shall want Provisions or any Assistance whatever is to say nothing while we are Masters of the Seas and can in four Hours come from Dover to Bologn, with Supplies of all Sorts, a passage so easie that you might bake his very Bread for him in Kent if you pleas'd."
         self.s3 = 'cgtagctagcgacgcagccaatcgatcg'
         self.s4 = 'cgagatgccaatagagagagcgctgta'
+        self.s5 = 'ab' * 70
+        self.s6 = 'bc' * 70
         alphabet = list(set(self.s1 + self.s2))
         self.k_slow = flakes.string.StringKernel(mode='slow', alphabet=alphabet)
         self.k_np = flakes.string.StringKernel(mode='numpy', alphabet=alphabet)
         self.k_tf = flakes.string.StringKernel(mode='tf', alphabet=alphabet, device=self.DEVICE)
         self.k_tf_batch = flakes.string.StringKernel(mode='tf-batch', alphabet=alphabet,
-                                                          device=self.DEVICE,
-                                                          trace=self.TRACE_FILE)
+                                                     device=self.DEVICE,
+                                                     trace=self.TRACE_FILE,
+                                                     batch_size=2)
 
     @unittest.skip('profiling')
     def test_prof_1(self):
@@ -107,7 +110,7 @@ class StringKernelProfiling(unittest.TestCase):
         print result2
         print after - before
 
-    #@unittest.skip('profiling')
+    @unittest.skip('profiling')
     def test_prof_gram_batch_1(self):
         self.k_tf_batch.order_coefs = [0.1, 0.2, 0.4, 0.5, 0.7, 1, 1, 1] + 32 * [1.0]
         self.k_tf_batch.gap_decay = 0.8
@@ -120,6 +123,20 @@ class StringKernelProfiling(unittest.TestCase):
         print "START PROF GRAM BATCH"
         X = [[self.s3]] * 100
         X2 = [[self.s4]] * 100
+        before = datetime.datetime.now()
+        result2 = self.k_tf_batch.K(X, X2)
+        after = datetime.datetime.now()
+        print result2
+        print after - before
+
+
+    def test_prof_gram_big_strings_batch_1(self):
+        self.k_tf_batch.order_coefs = [1.0] * 5
+        self.k_tf_batch.gap_decay = 0.8
+        self.k_tf_batch.match_decay = 0.8
+        
+        X = [[self.s5]] * 1000
+        X2 = [[self.s6]] * 1000
         before = datetime.datetime.now()
         result2 = self.k_tf_batch.K(X, X2)
         after = datetime.datetime.now()
