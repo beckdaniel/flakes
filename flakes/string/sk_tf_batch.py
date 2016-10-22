5d0e982586ec58285aaad0529e94b52187ac28d5
beckdaniel <daniel.e.beck@gmail.com>
2016-10-22 14:36:17 +0100

    refactored padding + code cleaning

--- flakes/string/sk_tf_batch.py
+++ flakes/string/sk_tf_batch.py
@@ -317,29 +317,21 @@ class TFBatchStringKernel(object):
         # params[2] should be always order_coefs
         order = len(params[2])
 
-        # We check input lengths and pad them with zeros.
-        # At Gram matrix time, we skip checking the length.
+        # Check input length, unlesse we are in Gram matrix mode.
         if gram:
             if not self.gram_mode:
                 maxlen = max([len(x[0]) for x in X])
                 self.gram_mode = True
             else:
                 maxlen = self.maxlen
-            #X = [self._pad(x[0], maxlen) for x in X]
-            #X2 = X
-            #indices = [[i1, i2] for i1 in range(len(X)) for i2 in range(len(X2)) if i1 >= i2]
             indices = [[i1, i2] for i1 in range(len(X)) for i2 in range(len(X)) if i1 >= i2]
         else:
             self.gram_mode = False
             if diag:
                 maxlen = max([len(x[0]) for x in X])
-                #X = [self._pad(x[0], maxlen) for x in X]
-                #X2 = X
                 indices = [[i1, i1] for i1 in range(len(X))]
             else:
                 maxlen = max([len(x[0]) for x in list(X) + list(X2)])
-                #X = [self._pad(x[0], maxlen) for x in X]
-                #X2 = [self._pad(x[0], maxlen) for x in X2]
                 indices = [[i1, i2] for i1 in range(len(X)) for i2 in range(len(X2))]
 
         # We have to rebuild the graph if we have new inputs
@@ -352,6 +344,8 @@ class TFBatchStringKernel(object):
                 self.sess.close()
                 self.sess = None
 
+        # We also have to pad inputs so each instance
+        # has maximum length.
         X = [self._pad(x[0], self.maxlen) for x in X]
         if gram or diag:
             X2 = X
@@ -359,10 +353,6 @@ class TFBatchStringKernel(object):
             X2 = [self._pad(x[0], self.maxlen) for x in X2]
 
         # Initialize return values
-        #k_results = [] 
-        #gap_grads = []
-        #match_grads = []
-        #coef_grads = []
         k_results = np.zeros((len(X), len(X2)))
         gap_grads = np.zeros((len(X), len(X2)))
         match_grads = np.zeros((len(X), len(X2)))
@@ -411,7 +401,6 @@ class TFBatchStringKernel(object):
                 trace = tl.generate_chrome_trace_format()
                 with open(self.trace, 'w') as f:
                     f.write(trace)
-            #for j in xrange(self.BATCH_SIZE):
 
             # Populate the return matrices:
             for j, item in enumerate(items):
@@ -419,99 +408,42 @@ class TFBatchStringKernel(object):
                 gap_grads[item[0], item[1]] = gapg[j]
                 match_grads[item[0], item[1]] = matchg[j]
                 coef_grads[item[0], item[1]] = coefsg[:, j]
-                #k_results.append(k[j])
-                #gap_grads.append(gapg[j])
-                #match_grads.append(matchg[j])
-                #coef_grads.append(coefsg[:, j])
-        
+
+        # Postprocess the matrices and return results.
         if gram:
-            # symmetrize
+            # Symmetrize results
             new_coef_grads = np.zeros((len(X), len(X), order))
             for i in xrange(order):
                 new_coef_grads[:, :, i] = self._symmetrize(coef_grads[:, :, i])
             return (self._symmetrize(k_results), self._symmetrize(gap_grads),
                     self._symmetrize(match_grads), new_coef_grads)
         elif diag:
+            # Get only the diagonals (the other elements are zeroes)
             new_coef_grads = np.zeros((len(X), order))
             for i in xrange(order):
                 new_coef_grads[:, i] = np.diag(coef_grads[:, :, i])
             return (np.diag(k_results), np.diag(gap_grads),
                     np.diag(match_grads), new_coef_grads)
         else:
+            # Return full results
             return k_results, gap_grads, match_grads, coef_grads
 
-
-        # Reshape the return values since they are vectors:
-        k_results = self._reshape_result(k_results, len(indices), len(X), len(X2), gram, diag)
-        gap_grads = self._reshape_result(gap_grads, len(indices), len(X), len(X2), gram, diag)
-        match_grads = self._reshape_result(match_grads, len(indices), len(X), len(X2), gram, diag)
-        for i in xrange(order):
-            coef_grads[:, :, i] = self._reshape_result(coef_grads[:, :, i], len(indices), len(X), len(X2), gram, diag)
-
-        return k_results, gap_grads, match_grads, coef_grads
-        
-
-
-        if not diag:
-            if gram:
-                lenX2 = None
-            else:
-                lenX2 = len(X2)
-            k_results = self._triangulate(k_results, len(indices), len(X), lenX2)
-            gap_grads = self._triangulate(gap_grads, len(indices), len(X), lenX2)
-            match_grads = self._triangulate(match_grads, len(indices), len(X), lenX2)
-            coef_grads = self._triangulate(coef_grads, len(indices), len(X), lenX2)
-        else:
-            k_results = np.array(k_results[:len(X)])
-            gap_grads = np.array(gap_grads[:len(X)])
-            match_grads = np.array(match_grads[:len(X)])
-            coef_grads = np.array(coef_grads[:len(X)])
-        return k_results, gap_grads, match_grads, coef_grads
-
     def _symmetrize(self, matrix):
+        """
+        Symmetrize a lower triangular matrix with
+        non-zero diagonal. Called when the kernel
+        is in Gram matrix mode.
+        """
         return matrix + matrix.T - np.diag(np.diag(matrix))
 
-    def _reshape_result(self, result, indlen, lenX, lenX2, gram, diag):
-        """
-        Transform the result vectors from the graph into their
-        original matrix form. Notice that we assume the result
-        vector is in the proper format, which is derived from
-        the indices vector in the K_unnorm method.
-        """
-        # We might have padded results that need pruning.
-        #result = result[:indlen]
-        if gram:
-            tril = np.zeros((lenX, lenX))
-            tril[np.tril_indices(lenX, 0)] = result
-            # symmetrization
-            return tril + tril.T - np.diag(np.diag(tril))
-        elif diag:
-            return np.array(result)
-        else:
-            return np.reshape(result, (lenX, lenX2))
-
-    def _triangulate(self, vector, indices, lenX, lenX2=None):
-        """
-        Transform the return vectors from the graph into their
-        original matrix form.
-        """
-        vector = np.array(vector)
-        if lenX2 == None:
-            lenX2 = lenX
-            gram = True
-        else:
-            gram = False
-        if vector[0].shape == ():
-            result = np.zeros((lenX, lenX2))
-        else:
-            result = np.zeros((lenX, lenX2, vector[0].shape[0]))
-        for i, elem in enumerate(indices):
-            result[elem[0], elem[1]] = vector[i]
-            if elem[0] != elem[1] and gram:
-                result[elem[1], elem[0]] = vector[i]
-        return result
-
     def _pad(self, s, length):
+        """
+        Ensure a string has maximum length by adding
+        zeroes. Notice we assume zero as the index
+        for a zero embedding so the kernel result
+        is not affected by padding.
+        """
         new_s = np.zeros(length)
         new_s[:len(s)] = s
         return new_s
+
